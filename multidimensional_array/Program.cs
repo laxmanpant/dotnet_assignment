a6bea512e67e48e2683a4d91743ec2b28723be9f
laxman pant <laxmanpant@kbc.edu.np>
2026-07-15 20:40:35 +0545

    new file

---
+++ multidimensional_array/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multidimensional_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         int[,] = matrix int[2,2]
+                {
+                { 1,2},{ 1,2};
+            }
+        }
+    }
+}
